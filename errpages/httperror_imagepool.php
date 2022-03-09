75c5a1c8a1d13af4e04a05da7c64a7d0b1e93752
jxmot <webdev@stupiddog.net>
2022-03-09 10:48:12 -0600

    initial commit, image pool and randomizer

---
+++ errpages/httperror_imagepool.php
@@ -0,0 +1,16 @@
+<?php
+/* ********************************************************
+    Error Page Image Pool
+
+    The CSS class `.site-bg-image` MUST remain in the style
+    tag within `httperror.php`. It contains a PHP snippet 
+    that is used for accessing `$selectedBg` and inserting 
+    the image file name.
+*/
+$bg = array(_BASE_PATH . '/errpages/accidental-slip-542551_1280.jpg', 
+            _BASE_PATH . '/errpages/pexels-george-becker-374918.jpg', 
+            _BASE_PATH . '/errpages/mistake-876597_1280.jpg', 
+            _BASE_PATH . '/errpages/mistake-2344150_1280.jpg');
+$i = rand(0, count($bg)-1);
+$selectedBg = "$bg[$i]";
+?>
